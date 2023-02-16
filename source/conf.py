339516d28383907fb6f8b0ac1d81eb49b58a19b7
Mohammed Al Farhan <alfarhanmah@gmail.com>
2023-02-16 10:19:59 +0300

    update the config to use materialt

--- source/conf.py
+++ source/conf.py
@@ -22,9 +22,16 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+html_theme = 'sphinx_material'
+
+# import sphinx_rtd_theme
+
+# html_theme = "sphinx_rtd_theme"
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 # html_theme = 'sphinx_rtd_theme'
 # html_theme = 'python_docs_theme'
-html_theme = 'classic'
+# html_theme = 'classic'
 
 # html_theme_options = { "logo_only": True }
 
