a367d0c0ebe46566de02d82213b77ff9cbb38029
Mohammed Al Farhan <alfarhanmah@gmail.com>
2023-03-02 11:23:32 +0300

    Update run.sh

--- shaheen2/hpl/run.sh
+++ shaheen2/hpl/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {5100..5103} {5820..5823}
+for i in {1852..1855} {3324..3327} {6448..6451} {6512..6515} {6740..6743}
 do
-  sbatch -w nid0$i linpack_slurm.sh
+    sbatch -w nid0$i linpack_slurm.sh
 done
