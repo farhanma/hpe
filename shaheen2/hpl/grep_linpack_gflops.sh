65610f5f8e089f037a6eb57eed8b303e4dd8f431
Mohammed Al Farhan <alfarhanmah@gmail.com>
2023-03-06 10:14:44 +0300

    remove extra things

--- shaheen2/hpl/grep_linpack_gflops.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in `ls linpack-*nid*.out`
-do
-    grep "NODE ID" $i | awk '{print $3}' | tr '\n' ' '
-    grep -A 3 "Performance Summary" $i | tail -n 1 | awk '{print $4}' | tr '\n' ' '
-    echo
-done
