ab086aedbf329ae8aa3f0cfd8114e6054dbb2341
Mohammed Al Farhan <alfarhanmah@gmail.com>
2023-03-07 12:25:40 +0300

    Add IOR for E1000

---
+++ shaheen2/ior/e1000/ior-e1000_slurm.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+LAUNCHER_NAME="ior-launcher-rw.sh"
+
+if [ ! -x $LAUNCHER_NAME ]
+then
+   echo "LAUNCHER_NAME=$LAUNCHER_NAME doesn't exist"
+   exit 1
+fi
+
+SLURM_PART='workq'
+SLURM_ACCOUNT='v1003'
+SLURM_RESERVATION='rwproject2'
+
+if [ ! -z "$SLURM_RESERVATION" ]
+then
+	SLURM_RES_ARG="--reservation=$SLURM_RESERVATION"
+else
+	unset SLURM_RES_ARG
+fi
+
+if [ ! -z "$SLURM_ACCOUNT" ]
+then
+	SLURM_ACC_ARG="--account=$SLURM_ACCOUNT"
+else
+	unset SLURM_ACC_ARG
+fi
+
+NODES=128
+PPN=32
+NP=$(( $NODES * $PPN ))
+
+RESULTS_DIR=./RESULTS
+
+if [ ! -d $RESULTS_DIR ]
+then
+	mkdir $RESULTS_DIR
+fi
+
+OUT_FPATH=./output.`date +"%Y%m%d-%H%M%S"`
+
+rm -rf ${OUT_FPATH} && mkdir -p ${OUT_FPATH}
+
+OUTFILE=${OUT_FPATH}/out-ior-${NODES}-nodes-${NODES}x${PPN}-$NP-procs-job-%J.ior
+
+sbatch \
+	--comment="$TEST_NAME" --cpu-freq=Performance --hint=compute_bound \
+	-d singleton --exclusive -J "ior_pj" -m block  --nodes=$NODES \
+	--ntasks-per-node=$PPN -p $SLURM_PART $SLURM_RES_ARG --wait-all-nodes=1 \
+	--time=01:00:01 $SLURM_ACC_ARG -o $OUTFILE ./$LAUNCHER_NAME
