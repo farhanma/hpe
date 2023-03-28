50957a3ec353be229e0be713353ba89666173f35
Mohammed Al Farhan <alfarhanmah@gmail.com>
2023-03-28 16:00:34 +0300

    Upload the file sshd

--- shaheen2/trimming/workload/tests/slurm/xtbte_perf_batch_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sbcast --compress=lz4 /opt/cray/diag/bin/xtbte_perf /tmp/xtbte_perf
-sleep 10
-LOG_FILE=$WTS_JOB_LOG
-srun -n $SLURM_JOB_NUM_NODES --ntasks-per-node=1 /tmp/xtbte_perf -B 6 -N 2 -l 2 -S 0x7 -f /tmp >> $WTS_JOB_LOG 
-
--- shaheen2/trimming/workload/tests/slurm/xtfma_amo_batch_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sbcast --compress=lz4 /opt/cray/diag/bin/xtfma_amo /tmp/xtfma_amo
-sleep 10
-LOG_FILE=$WTS_JOB_LOG
-srun -n $SLURM_JOB_NUM_NODES --ntasks-per-node=1 /tmp/xtfma_amo -R 1 -l 2 -N 1 -M 0 >> $LOG_FILE 
-
